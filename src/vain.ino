34360e099938eb589080a8bf8f52fa9ad0b7fa2c
Danny Dover <136573+dannydover@users.noreply.github.com>
2017-07-24 00:15:46 -0700

    added refresh token logic

--- src/vain.ino
+++ src/vain.ino
@@ -6,11 +6,12 @@
 
  /*  TO DO
 
-    * Write code to refresh token after indicated expiration
+    * Display real-time vistors number on external display
+    * Fix bug that appears to be preventing code from executing after line 176
+    * Fix bug of when number of users gets stuck at last value when value switches to 0
     * Extract client_id and store as part of customer data? I don't want it stored in the generic webhook
     * Implement more robust error handling, what happens if Google API requests don't return expected results?
     * Create easier to understand naming convention for methods/webhooks
-    * Display real-time vistors number on external display
 
 
  */
@@ -20,17 +21,20 @@
  String accessToken = "";
  String refreshToken = "";
  int realTimeActiveUsers = 88;
- int tokenExpirationInSeconds = 0;
+ unsigned long tokenExpirationTime = 0;
  bool authenticated = false;
+ int pollingRate = 30000; //30 seconds expressed in milliseconds
+ int requestCounter = 0;
 
  void setup() {
      Particle.subscribe("initiateGAuthResponse", parseAuthResponse, MY_DEVICES);
      Particle.subscribe("gAuthPollResponse", attemptToAuthDevice, MY_DEVICES);
      Particle.subscribe("hook-error/pollGAuthForAuth", attemptToAuthDeviceErrors, MY_DEVICES);
      Particle.subscribe("realTimeActiveUsers", howManyRealTimeUsers, MY_DEVICES);
+     Particle.subscribe("refreshAccessTokenResponse", requestRefreshTokenResponse, MY_DEVICES);
 
      Serial.begin(9600);
-     Serial.println("Serial Output is working");
+     Serial.println("Status: Serial output is operational...");
 
      /* Device authenticates with Google Analytics API using following protocol:
         https://developers.google.com/identity/protocols/OAuth2ForDevices
@@ -54,7 +58,7 @@
 
      //Step 4 - Your application starts polling Google's authorization server to determine whether the user has authorized your app.
      while (authenticated == false) {
-         Serial.println("Polling Google authorization service");
+         Serial.println("Status: Polling Google authorization service...");
 
          Particle.publish("pollGAuthForAuth", deviceCode, PRIVATE);
          delay(20000);
@@ -76,16 +80,28 @@
      // After authenticating, request amount of real time active users from Google Analytics API every 30 seconds
      if (authenticated == true) {
          Particle.publish("getGARealTimeActiveUsers", accessToken, PRIVATE);
+         requestCounter++;
          Serial.print("Real Time Active Users: ");
          Serial.println(realTimeActiveUsers);
 
-         delay(30000);
+         // If access token is going to expire, request a new one from Google.
+         if ((requestCounter * pollingRate) > (tokenExpirationTime * 1000)) {
+            Particle.publish("requestAccessTokenRefresh", refreshToken, PRIVATE);
+
+            /* Debugging Code
+            Serial.print("requestCounter * pollingRate : ");
+            Serial.println(requestCounter * pollingRate);
+
+            Serial.print("tokenExpirationTime * 1000 : ");
+            Serial.println(tokenExpirationTime * 1000);
+
+            Serial.print("refreshToken : ");
+            Serial.println(refreshToken);
+            */
+         }
+
+         delay(pollingRate); //default is 30 seconds
      }
-
-     // TO CODE: Keep track of seconds passed (number of times API requested) and periodically use refresh token to request new access token
-
-
-
 
      // TO CODE: Display real-time vistors number on external display
 
@@ -112,7 +128,7 @@
 
 
  void attemptToAuthDevice(const char *event, const char *data) {
-     String token_type = "";
+     String tokenType = "";
 
      Serial.println("Status: Received successful response from Google's Auth Service after user input");
 
@@ -125,20 +141,22 @@
      char strBuffer[250] = "";
      str.toCharArray(strBuffer, 250);
      accessToken = strtok(strBuffer, "~");
-     token_type = strtok(NULL, "~");
+     tokenType = strtok(NULL, "~");
      String expiration = strtok(NULL, "~");
      refreshToken = strtok(NULL, "~");
 
-     tokenExpirationInSeconds = expiration.toInt();
+     //Manually subtract 10 minutes from received token expiration time in order
+     //to give time to get new access token. This line also coverts string to int.
+     tokenExpirationTime = expiration.toInt() - 600;
 
      Serial.print("accessToken: ");
      Serial.println(accessToken);
 
-     Serial.print("token_type: ");
-     Serial.println(token_type);
+     Serial.print("tokenType: ");
+     Serial.println(tokenType);
 
-     Serial.print("tokenExpirationInSeconds: ");
-     Serial.println(tokenExpirationInSeconds);
+     Serial.print("tokenExpirationTime: ");
+     Serial.println(tokenExpirationTime);
 
      Serial.print("refreshToken: ");
      Serial.println(refreshToken);
@@ -148,7 +166,6 @@
          Serial.println("Authenticated: true");
      }
  }
-
 
  void howManyRealTimeUsers(const char *event, const char *data) {
      Serial.println("Status: Received amount of Real Time Active Users: ");
@@ -164,4 +181,22 @@
        dataString.toCharArray(carray, sizeof(carray));
        realTimeActiveUsers = atoi(carray);
     }
+ }
+
+ void requestRefreshTokenResponse(const char *event, const char *data) {
+   String tokenType = "";
+
+   Serial.println("Status: Received response from request to refresh access token: ");
+   Serial.println(data);
+
+   //Split JSON payload string into global variables (split is by "~")
+   String str = String(data);
+
+   char strBuffer[250] = "";
+   str.toCharArray(strBuffer, 250);
+   accessToken = strtok(strBuffer, "~");
+   tokenType = strtok(NULL, "~");
+   String expiration = strtok(NULL, "~");
+
+   requestCounter = 0;
  }
